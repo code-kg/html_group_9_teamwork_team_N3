cf12d7b5945ceca86f83654e99db739d1b74f512
Timurlan <stellfalx@gmail.com>
2017-05-07 12:09:48 +0600

    modifing main index page

--- js/core.js
+++ js/core.js
@@ -11,6 +11,53 @@ $(document).ready(function () {
     });
 });
 
+//main slider
+var slideIndex = 0;
+var slides = document.getElementsByClassName('slide');
+var dots = document.getElementsByClassName('dot');
+
+function turnSlide(n) {
+    showSlide(slideIndex = slideIndex + n);
+}
+
+showSlide(slideIndex);
+function showSlide(n) {
+    var x;
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' current', '');
+    }
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' current';
+}
+
+//autorun for slider
+autoPlaySlide();
+function autoPlaySlide() {
+    var slides = document.getElementsByClassName('slide');
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' current', '');
+    }
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' current';
+    setTimeout(autoPlaySlide, 3000);
+}
+
+//hide/show menu
+$(document).ready(function(){
+    $('#menu-btn').click(function(){
+        $('.menu').slideToggle(700);
+    });
+});
 
 // carousel effect
 var slideNumber = 1;
